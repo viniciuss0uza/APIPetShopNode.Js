459f50584774f41fbdae0d89ec176bc554a60eff
Vinícius <vinicius.m.sousa3009@gmail.com>
2024-10-25 21:49:32 -0300

    Criado a API Pet Shop

---
+++ index.js
@@ -0,0 +1,22 @@
+// Não é mais tudo !!!!
+const express = require('express')
+const routers = require('./src/routes/pessoa')
+const database = require('./src/config/database')
+// Instanciar um express
+const app = express()
+app.use(express.json())
+app.use(routers)
+const PORT = 3000
+
+database.db
+    .sync({ force: false })
+    .then((_) => {
+        console.info("Banco conectado com sucesso!!")
+        app.listen(PORT, () => {
+            console.info(`Servidor rodando na porta ${PORT}`)
+         
+        })
+    })
+    .catch((e) => {
+        console.error('Conexão falhou: ${e}')
+    })
